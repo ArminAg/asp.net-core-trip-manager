3fdd8f7456df66fd6e2bfed8a64dff4d4004a8c6
Armin <armin.ag@outlook.com>
2017-03-17 14:26:49 +0100

    Add Form for adding Stops to map

--- src/asp.net-core-trip-manager/wwwroot/js/tripEditorController.js
+++ src/asp.net-core-trip-manager/wwwroot/js/tripEditorController.js
@@ -12,8 +12,11 @@
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+        vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function (response) {
                 // Success
                 angular.copy(response.data, vm.stops);
@@ -25,6 +28,24 @@
             .finally(function () {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function () {
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+                    // Success
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function () {
+                    // Failure
+                    vm.errorMessage = "Failed to add new stop";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        };
     }
 
     function _showMap(stops) {
