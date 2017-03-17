9ab7b14285e8959679e8ce9b3c3ee8318c4c1c66
Armin <armin.ag@outlook.com>
2017-03-17 02:08:31 +0100

    Add Page for My Trips

---
+++ src/asp.net-core-trip-manager/wwwroot/js/simpleControls.js
@@ -0,0 +1,18 @@
+﻿
+(function () {
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            scope: { // Visible inside the template
+                show: "=displayWhen" // Use from the consumer of directive
+            },
+            restrict: "E",
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+
+})();
